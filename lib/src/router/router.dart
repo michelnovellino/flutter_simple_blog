4e446ed604c801774fb4aad4aae67b3255f6d88e
michel novellino <michelnovellino.programador@gmail.com>
2020-02-25 11:40:37 -0400

    Router inicial, pagina de blog y home, solo creacion

---
+++ lib/src/router/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutte_simple_blog/src/pages/blog.page.dart';
+import 'package:flutte_simple_blog/src/pages/home.page.dart';
+import 'package:flutter/material.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/blog':
+        return MaterialPageRoute(builder: (_) => BlogPage());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                      child: Text('No route defined for ${settings.name}')),
+                ));
+    }
+  }
+}
